0dfbab426d2ea88bb5d65b0f46bfb329cb305445
Skinner Cheng <skinner.cheng@gmail.com>
2013-04-18 17:27:06 -0700

    Replace the title of pages "Skinner's Micro Search Demo" with "Stop Search".

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Skinner's Micro Search Demo"
+    base_title = "Stop Search"
     if page_title.empty?
       base_title
     else
